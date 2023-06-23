6e47a32317090b925d490f30c12338f41c759c91
Wilson Phillips <WilsonEPhillips@hotmail.com>
2023-06-23 07:56:42 -0500

    Update ScreenOff.sh

--- ScreenOff.sh
+++ ScreenOff.sh
@@ -3,4 +3,3 @@
 # This script will immediately put the monitor into powersave mode.
 # Any mouse movement or keystroke will power the monitor on again.
 
-/bin/sleep 1 && /bin/dbus-send --session --print-reply --dest=org.kde.kglobalaccel  /component/org_kde_powerdevil org.kde.kglobalaccel.Component.invokeShortcut string:'Turn Off Screen'
