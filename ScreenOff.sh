c671964354087b30b04b6d3105ac438ffe467c13
Wilson Phillips <WilsonEPhillips@hotmail.com>
2023-07-31 11:20:06 -0500

    Update comments

--- ScreenOff.sh
+++ ScreenOff.sh
@@ -2,5 +2,9 @@
 
 # This script will immediately put the monitor into powersave mode.
 # Any mouse movement or keystroke will power the monitor on again.
+# It must be placed in the $PATH and be set to executable.
+# You can run it from a shortcut on your desktop if needed.
+# This only works in a Wayland Plasma session.
+
 
 /bin/sleep 3 && /bin/dbus-send --session --print-reply --dest=org.kde.kglobalaccel  /component/org_kde_powerdevil org.kde.kglobalaccel.Component.invokeShortcut string:'Turn Off Screen'
